60185ded1d32c9ae13d3bcd7d9be08d0b0f78174
Nicholas Stevens <nstevens@nanick.org>
2021-06-04 08:59:23 -0500

    Added a unit test for cookie parsing.

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -198,5 +198,35 @@ namespace UnitTest1
             string exp_compare = Encoding.UTF8.GetString(Encoding.UTF8.GetBytes(returnFiles.GetProperty("file").GetString()));
             Assert.IsTrue(control_compare.Equals(exp_compare));
         }
+        [TestMethod]
+        public void TestCookieParsing()
+        {
+
+            OrderedDictionary headers = new OrderedDictionary();
+            headers.Add("method", "GET");
+            headers.Add("authority", "www.facebook.com");
+            headers.Add("scheme", "https");
+            headers.Add("path", "/");
+            headers.Add("pragma", "no-cache");
+            headers.Add("cache-control", "no-cache");
+            headers.Add("dnt", "1");
+            headers.Add("upgrade-insecure-requests", "1");
+            headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36");
+            headers.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            headers.Add("sec-fetch-site", "none");
+            headers.Add("sec-fetch-mode", "navigate");
+            headers.Add("sec-fetch-user", "?1");
+            headers.Add("sec-fetch-dest", "document");
+            headers.Add("sec-ch-ua", "\" Not; A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"");
+            headers.Add("sec-ch-ua-mobile", "?0");
+            headers.Add("accept-encoding", "gzip, deflate");
+            headers.Add("accept-language", "en-US,en;q=0.9");
+            RetObject r = Execute.HttpRequest.Send(
+                "https://www.facebook.com/",
+                HttpMethod.Get,
+                headers
+            );
+            Assert.IsTrue(r.CookieCollection.Count > 0);
+        }
     }
 }
